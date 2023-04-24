2fd1a4ad1e824974e370d7f8eccb429349e1ce80
joeychan916 <129896539+joeychan916@users.noreply.github.com>
2023-04-23 18:10:01 -0700

    packing list for camping

---
+++ packing-list.js
@@ -0,0 +1,20 @@
+const packingList = ["bowls", "plates", "pots", "pans", "eating utensils", "glasses", "cups", "cooking utensils"];
+undefined
+> 
+> console.log("Kitchen stuff to pack:");
+Kitchen stuff to pack:
+undefined
+> for (let i = 0; i < packingList.length; i++) {
+...   console.log(packingList[i]);
+... }
+bowls
+plates
+pots
+pans
+eating utensils
+glasses
+cups
+cooking utensils
+undefined
+> packingList[3];
+'pans'
