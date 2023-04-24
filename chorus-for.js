7192e53c61795315d9f124a3a5b7656bff7a06bc
joeychan916 <129896539+joeychan916@users.noreply.github.com>
2023-04-23 18:41:23 -0700

    Looping and Repetition: For Loops

---
+++ chorus-for.js
@@ -0,0 +1,18 @@
+const chorus = "Let's dance!";
+SyntaxError: Identifier 'chorus' has already been declared
+for (let repeat = 0; repeat < 10; repeat++) {
+...   console.log(chorus);
+... }
+Let's dance!
+Let's dance!
+Let's dance!
+Let's dance!
+Let's dance!
+Let's dance!
+Let's dance!
+Let's dance!
+Let's dance!
+Let's dance!
+
+console.log("Until the sun comes up!");
+Until the sun comes up!
