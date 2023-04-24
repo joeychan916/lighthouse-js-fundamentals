ebe9581fbf20a979d1bb1c518ac79d897af37644
joeychan916 <129896539+joeychan916@users.noreply.github.com>
2023-04-23 18:35:37 -0700

    Odd Checker

---
+++ odd.js
@@ -0,0 +1,6 @@
+const isOdd = function (num) {
+  return num % 2 !== 0;
+};
+
+console.log("3 is odd: " + isOdd(3)); // Output: 3 is odd: true
+console.log("8 is odd: " + isOdd(8)); // Output: 8 is odd: false
